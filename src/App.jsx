eff48a6789f67148fd577a5d6524bffb9a9e4486
suhasini2000 <you@example.com>
2025-05-18 18:10:05 +0530

    uploaded to master

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import UserList from './UserList'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React + JSON Server</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UserList.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function UserList() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetch("https://student-json-api.netlify.app/db.json")
+      .then(res => res.json())
+      .then(data => setStudents(data.students))
+      .catch(err => console.error("Error loading data:", err));
+  }, []);
+
+  return (
+    <div>
+      <h2>Student List</h2>
+      <ul>
+        {students.map((stu, index) => (
+          <li key={index}>{stu.name} - {stu.course}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
