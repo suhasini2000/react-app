96927b6bb19f21b4c752d3057eec52c3a8281401
suhasini2000 <you@example.com>
2025-05-18 22:01:36 +0530

    master

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -5,7 +5,7 @@ function UserList() {
   const [error, setError] = useState("");
 
   useEffect(() => {
-  fetch("https://corsproxy.io/?https://tudent-json-api.netlify.app/db.json")
+  fetch("/students.json")
     .then(res => res.json())
     .then(data => setStudents(data.students || []))
     .catch(() => setError("Failed to load students."));
